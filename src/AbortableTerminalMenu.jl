cb837ccb061ac7cf2c45cc9ed1b651a2e087ffb5
Eben60 <norealmail@nowhere.org>
2025-06-01 21:51:56 +0200

    actually initial commit

--- src/AbortableTerminalMenu.jl
+++ src/AbortableTerminalMenu.jl
@@ -1,3 +1,12 @@
+
+"""
+    Package AbortableTerminalMenu v$(pkgversion(AbortableTerminalMenu))
+
+
+
+Documentation: 
+$(isnothing(get(ENV, "CI", nothing)) ? ("\n" * "Package local path: " * pathof(AbortableTerminalMenu)) : "")
+"""
 module AbortableTerminalMenu
 
 # Write your package code here.
